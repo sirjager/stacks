588a930760dd6b237e3e23a70891d23179c05972
Ankur Kumar <kankurcool@gmail.com>
2023-12-10 18:01:48 +0530

    seperated nginx and portainer

--- traefik/genpass.sh
+++ traefik/genpass.sh
@@ -2,6 +2,6 @@
 read -p "Enter username: " _user
 read -p "Enter password: " _pass
 
-hashed=$(openssl passwd -apr1 "$_pass" | sed 's/\$/\$$/g')
+hashed=$(openssl passwd -apr1 "$_pass" | sed 's/\$/\$/g')
 
 echo "$hashed" | xclip -selection clipboard
